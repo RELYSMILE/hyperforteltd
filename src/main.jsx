8f1c05b60424dd51f0d6f98feee886a80409fe0c
RELYSMILE <relysmiled@gmail.com>
2025-06-01 16:32:16 +0100

    Your update code s

--- src/main.jsx
+++ src/main.jsx
@@ -5,35 +5,20 @@ import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './Screens/Home.jsx'
 import Login from './Screens/Login.jsx'
-import App from './App.jsx'
 
 const a = 1
 
 const router = createBrowserRouter([
   {
-    path: '/home',
+    path: '/',
     element: <App />,
     errorElement: <div>Not found</div>
   },
 ])
-const route = createBrowserRouter([
-  {
-    path: '/',
-    element: <Login />,
-    errorElement: <div>Not found</div>
-  },
-  {
-    path: '/login',
-    element: <Login />,
-    errorElement: <div>Not found</div>
-  },
-])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {a === 1 ?
-      <RouterProvider router={ router} />
-    :
-    <RouterProvider router={ route} />}
+    <RouterProvider router={ router} />
   </React.StrictMode>,
 )
